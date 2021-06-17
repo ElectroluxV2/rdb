02a40ca466781e15a25e752af315b97f12d2f11a
Mateusz Budzisz <ElectroluxV2@gmail.com>
2021-06-17 10:32:51 +0200

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,11 +1,13 @@
-create table if not exists clubs (
+drop table if exists clubs cascade;
+create table clubs (
     club_id int not null auto_increment,
     short_name varchar(64) not null,
     full_name varchar(256) not null,
     primary key (club_id)
 );
 
-create table if not exists sailors (
+drop table if exists sailors cascade;
+create table sailors (
     sailor_id int not null auto_increment,
     club_id int not null,
     given_name varchar(128) not null,
@@ -15,7 +17,8 @@ create table if not exists sailors (
     constraint fk_sailors_clubs_club_id foreign key (club_id) references clubs(club_id) on delete cascade
 );
 
-create table if not exists regattas (
+drop table if exists regattas cascade;
+create table regattas (
     regatta_id int not null auto_increment,
     name varchar(512) not null,
     year int not null,
@@ -23,7 +26,8 @@ create table if not exists regattas (
     primary key (regatta_id)
 );
 
-create table if not exists starting_list (
+drop table if exists starting_list cascade;
+create table starting_list (
     entry_id int not null auto_increment,
     sailor_id int not null,
     regatta_id int not null,
@@ -34,23 +38,37 @@ create table if not exists starting_list (
     constraint fk_starting_list_regattas_regatta_id foreign key (regatta_id) references regattas(regatta_id) on delete cascade
 );
 
-create table if not exists races (
+drop table if exists races cascade;
+create table races (
     race_id int not null auto_increment,
     regatta_id int not null,
-    name int not null,
+    number int not null,
+    name tinytext,
     primary key (race_id),
     constraint fk_races_regattas_regatta_id foreign key (regatta_id) references regattas(regatta_id) on delete cascade
 );
 
-create table if not exists race_results (
-    race_id int not null,
-    sail_number varchar(10) not null,
-    place int not null,
-    primary key (race_id, sail_number),
-    constraint fk_race_results_races_race_id foreign key (race_id) references races(race_id) on delete cascade
+drop table if exists results_abbreviations cascade;
+create table results_abbreviations (
+    short_name varchar(5) not null unique,
+    full_name varchar(255) not null,
+    primary key (short_name)
 );
 
-create table if not exists year_categories (
+drop table if exists race_results cascade;
+create table race_results (
+    race_id int not null,
+    sail_number varchar(10) not null,
+    place int not null ,
+    abbreviation varchar(5) default null,
+    points int,
+    primary key (race_id, sail_number),
+    constraint fk_race_results_races_race_id foreign key (race_id) references races(race_id) on delete cascade,
+    constraint fk_race_results_results_abbreviations_abbreviation foreign key (abbreviation) references results_abbreviations(short_name) on delete cascade
+);
+
+drop table if exists year_categories cascade;
+create table year_categories (
   category varchar(16) not null unique,
   younger_than int not null,
   primary key (category)
@@ -61,7 +79,7 @@ drop function if exists get_age_from_birthdate;
 create function if not exists get_age_from_birthdate(birth_date datetime) returns integer
 begin
     return year(current_timestamp) - year(birth_date) - (right(current_timestamp, 5) < right(birth_date, 5));
-end$$
+end; $$
 delimiter ;
 
 delimiter $$
@@ -73,7 +91,7 @@ begin
     set category_but_this_time_not_ambiguous = (select category from year_categories where younger_than > year order by younger_than limit 1);
 
     return category_but_this_time_not_ambiguous;
-end$$
+end; $$
 delimiter ;
 
 delimiter $$
@@ -81,7 +99,7 @@ drop function if exists get_category_from_birthdate;
 create function if not exists get_category_from_birthdate(birth_date datetime) returns varchar(16)
 begin
     return get_category_from_age(get_age_from_birthdate(birth_date));
-end$$
+end; $$
 delimiter ;
 
 delimiter $$
@@ -96,23 +114,65 @@ begin
            join starting_list on sailors.sailor_id = starting_list.sailor_id
            join clubs on clubs.club_id = sailors.club_id
     where starting_list.regatta_id = 1;
-end $$
+end; $$
 delimiter ;
 
 delimiter $$
-drop procedure if exists create_results_view_for_regatta;
-create procedure create_results_view_for_regatta(for_regatta_id integer)
+drop function if exists get_points_for_race_result;
+create function get_points_for_race_result(total_competitors integer, place integer, abbreviation varchar(5)) returns integer
 begin
-    declare name varchar(100);
-    set name = (concat('view_regatta_results_', for_regatta_id));
-    create view name as
-        select sum(place) as points
-        from race_results
-        join races on race_results.race_id = races.race_id
-        where regatta_id = 12
-        group by sail_number;
+    declare points integer;
 
-end$$
+    if abbreviation is null then
+        set points = place;
+    else
+        set points = total_competitors;
+    end if;
+
+    return points;
+
+end; $$
 delimiter ;
 
-create trigger create_results_view_on_regatta_insert after update on regattas for each row call create_results_view_for_regatta(regatta_id);
+delimiter $$
+drop function if exists get_regatta_id_from_race_id;
+create function if not exists get_regatta_id_from_race_id(race_id integer) returns integer
+begin
+    declare found_regatta_id varchar(16);
+
+    set found_regatta_id = (select regatta_id from races where races.race_id = race_id);
+
+    return found_regatta_id;
+end; $$
+delimiter ;
+
+delimiter $$
+drop function if exists get_total_competitors_by_regatta_id;
+create function if not exists get_total_competitors_by_regatta_id(regatta_id integer) returns integer
+begin
+    declare total_competitors varchar(16);
+
+    set total_competitors = (select count(sail_number) from starting_list where starting_list.regatta_id = regatta_id);
+
+    return total_competitors;
+end; $$
+delimiter ;
+
+delimiter $$
+drop procedure if exists update_points_for_race_result;
+create procedure update_points_for_race_result(new_place integer, new_abbreviation varchar(5), new_race_id integer)
+begin
+    declare total_competitors integer;
+    set total_competitors = get_total_competitors_by_regatta_id(get_regatta_id_from_race_id(new_race_id));
+
+    update race_results set points = get_points_for_race_result(total_competitors, new_place, new_abbreviation) where race_id = new_race_id and sail_number = new_abbreviation;
+end; $$
+delimiter ;
+
+drop trigger if exists calculate_points_for_race_result;
+create trigger calculate_points_for_race_result after insert on race_results for each row call update_points_for_race_result(new.place, new.abbreviation, new.race_id);
+
+drop trigger if exists recalculate_points_for_race_result;
+create trigger recalculate_points_for_race_result after update on race_results for each row call update_points_for_race_result(new.place, new.abbreviation, new.race_id);
+
+
